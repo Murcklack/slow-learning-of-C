302015cd0d79601ec4832b70cf5c545f17456708
Murcklack <dfcdg652@gmail.com>
2026-09-23 14:35:50 +0700

    Jhin 4

--- Программы/Язык-C/4 практика/zvezda.c
+++ Программы/Язык-C/4 практика/zvezda.c
@@ -1,12 +1,55 @@
 #include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int max(int a, int b){
+  return (a > b ? a : b);
+}
+
+int min(int a, int b){
+  return (a < b ? a : b);
+}
+
+
 int main(void) {
-  int t = 0, C = 20, A = 10, F = 10;
-  float speed = 0.1;
+  int C = 30, A = 15;
+  float speed = 0.2, F = 0.3, t=0;
 
   while (1) {
-    for (int y = 0; y < 50; y++) {
-      int x1 = C + A * sin(F * y + t);
-      int x2 = C - A * sin(F * y + t);
+    printf("\033[H"); // Возврат в верхний угол
+    for (int y = 0; y < 30; y++) { // Сколько строчек выплёвываем
+      int x1 = round(C + A * sin(F * y + t));
+      int x2 = round(C - A * sin(F * y + t));
+      
+      int L = min(x1, x2);
+      int R = max(x1, x2);
+
+      for (int i = 0; i < L; i++){
+        printf(" ");
+      }
+      int distance = R-L;
+      if (distance == 0){
+        printf("x\033[K\n"); // \033[K\n стирает остатки (очистка до конца строки)
+      }
+      
+      else {
+        int spaceBTW = distance - 1;
+        printf("0");
+                if (y % 2 == 0) {
+          printf("\033[32m");
+          for (int i = 0; i < spaceBTW; i++) {
+            printf("-");
+          }
+        } else {
+          printf("\033[33m"); 
+          for (int i = 0; i < spaceBTW; i++) {
+            printf("-");
+          }
+        }
+        printf("\033[0m 0 \033[K\n");
+      }
     }
-  }
+    t = t + speed; 
+    }
+  return 0;
 }
